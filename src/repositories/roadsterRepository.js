4941a5761e062c0abaf1766aca6b2ca59ee70a70
Jack Cornish <51884864+JJAC95@users.noreply.github.com>
2023-08-13 19:52:23 +0100

    Update roadsterRepository.js
    fixed function name to fix hook call error from useQuery

--- src/repositories/roadsterRepository.js
+++ src/repositories/roadsterRepository.js
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import fetchData from "../helpers/fetchData";
-const fetchElonsRoadster = () => {
+const FetchElonsRoadster = () => {
   return useQuery("roadster", () =>
     fetchData("https://api.spacexdata.com/v3/roadster", {
       staleTime: Infinity,
@@ -9,4 +9,4 @@ const fetchElonsRoadster = () => {
   );
 };
 
-export default fetchElonsRoadster;
+export default FetchElonsRoadster;
